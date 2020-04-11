61bf2420de5d8eaae745b7cbbf94423564b5bc32
Tashkent <17104706+ototashkent@users.noreply.github.com>
2020-04-11 08:06:19 -0400

    fuck commit messages

---
+++ IWETD.Game.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IWETD.Game.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ IWETD.Resources/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IWETD.Resources
+{
+    public class Class1
+    {
+    }
+}

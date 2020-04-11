16ad5d657ecd885508d8d7f3ad4eeaa0c597d150
Noah <altenyt@gmail.com>
2020-04-11 19:32:38 -0400

    Cache game files

--- IWETD.Game/IWETDGame.cs
+++ IWETD.Game/IWETDGame.cs
@@ -1,12 +1,25 @@
-﻿namespace IWETD.Game
+﻿using osu.Framework.Allocation;
+
+namespace IWETD.Game
 {
     public class IWETDGame : IWETDGameBase
     {
         private readonly string[] _args;
+        
+        private DependencyContainer _dependencies;
+
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) =>
+            _dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
 
         public IWETDGame(string[] args)
         {
             _args = args;
+        }
+
+        [BackgroundDependencyLoader]
+        private void Load()
+        {
+            _dependencies.CacheAs(this);
         }
     }
 }
--- IWETD.Game/IWETDGameBase.cs
+++ IWETD.Game/IWETDGameBase.cs
@@ -1,10 +1,26 @@
-﻿using osu.Framework.Graphics;
+﻿using IWETD.Resources;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.IO.Stores;
 
 namespace IWETD.Game
 {
     public class IWETDGameBase : osu.Framework.Game
     {
         protected override Container<Drawable> Content { get; }
+        
+        private DependencyContainer _dependencies;
+
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) => 
+            _dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
+
+        [BackgroundDependencyLoader]
+        private void Load()
+        {
+            Resources.AddStore(new DllResourceStore(IWETDResources.Assembly));
+            
+            _dependencies.Cache(this);
+        }
     }
 }

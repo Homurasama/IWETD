dedd060f9e8b184708359b33ed9b08fb3b80cd93
Noah <altenyt@gmail.com>
2020-04-11 19:28:05 -0400

    Add test scenes

---
+++ IWETD.Game.Tests/IWETDTestBrowser.cs
@@ -0,0 +1,14 @@
+﻿using osu.Framework.Testing;
+
+namespace IWETD.Game.Tests
+{
+    public class IWETDTestBrowser : IWETDGameBase
+    {
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            
+            Add(new TestBrowser("IWETD.Game.Tests"));
+        }
+    }
+}
---
+++ IWETD.Game.Tests/Visual/TestSceneIWETDGame.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Platform;
+using osu.Framework.Testing;
+using osuTK.Graphics;
+
+namespace IWETD.Game.Tests.Visual
+{
+    [TestFixture]
+    public class TestSceneIWETDGame : TestScene
+    {
+        private IReadOnlyList<Type> RequiredGameDependencies => new[]
+        {
+            typeof(IWETDGame)
+        };
+        
+        private IReadOnlyList<Type> RequiredGameBaseDependencies => new[]
+        {
+            typeof(IWETDGameBase)
+        };
+
+        [BackgroundDependencyLoader]
+        private void Load(GameHost host, IWETDGameBase gameBase)
+        {
+            IWETDGame game = new IWETDGame(null);
+            game.SetHost(host);
+            
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    Colour = Color4.Black,
+                    RelativeSizeAxes = Axes.Both
+                },
+                game
+            };
+            
+            AddUntilStep("wait for load", () => game.IsLoaded);
+            
+            AddAssert("check IWETDGame DI members", () =>
+            {
+                foreach (var type in RequiredGameDependencies)
+                {
+                    if (game.Dependencies.Get(type) == null)
+                        throw new InvalidOperationException($"{type} has not been cached");
+                }
+
+                return true;
+            });
+            
+            AddAssert("check IWETDGameBase DI members", () =>
+            {
+                foreach (var type in RequiredGameBaseDependencies)
+                {
+                    if (game.Dependencies.Get(type) == null)
+                        throw new InvalidOperationException($"{type} has not been cached");
+                }
+
+                return true;
+            });
+        }
+    }
+}

24289c9d97db594d06f863471a1732bd6c2f8329
Tashkent <17104706+ototashkent@users.noreply.github.com>
2020-04-11 21:50:43 -0400

    add configuration base

---
+++ IWETD.Game/Configuration/GameConfigManager.cs
@@ -0,0 +1,22 @@
+﻿using osu.Framework.Configuration;
+using osu.Framework.Platform;
+
+namespace IWETD.Game.Configuration
+{
+    public class GameConfigManager : IniConfigManager<IWETDConfig>
+    {
+        protected override void InitialiseDefaults()
+        {
+            base.InitialiseDefaults();
+        }
+        public GameConfigManager(Storage storage) : base(storage)
+        {
+
+        }
+    }
+
+    public enum IWETDConfig
+    {
+
+    }
+}

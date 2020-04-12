8fc116b3eb2c16481c26e4e2c80a9b0826455f20
Tashkent <17104706+ototashkent@users.noreply.github.com>
2020-04-12 04:10:44 -0400

    madoka moment

---
+++ IWETD.Game.Tests/Visual/TestGrid.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using IWETD.Game.Graphics.Graphs;
+using osuTK;
+
+namespace IWETD.Game.Tests.Graphics
+{
+    class TestGrid
+    {
+        private Grid _grid = new Grid(new Vector2(512), 32);
+
+        [Test]
+    }
+}
---
+++ IWETD.Game/Graphics/Graphs/Grid.cs
@@ -0,0 +1,35 @@
+﻿using IWETD.Game.IO;
+using IWETD.Game.Objects;
+using osuTK;
+
+namespace IWETD.Game.Graphics.Graphs
+{
+    public class Grid
+    {
+        public Vector2 Size;
+        public int CellCount = 0;
+        public int CellSize = 0;
+
+        public Store<DrawableGameObject> Objects = new Store<DrawableGameObject>();
+
+        public Grid(Vector2 scale, int cellSize = 1)
+        {
+            Size = scale;
+            CellCount = (int)(scale.X * scale.Y) / cellSize;
+            CellSize = cellSize;
+        }
+
+        public void Add(Vector2 pos, DrawableGameObject gameObject)
+        {
+            gameObject.X = GetProperPosition(pos).X;
+            gameObject.Y = GetProperPosition(pos).Y;
+
+            Objects.Add(gameObject);
+        }
+
+        public Vector2 GetProperPosition(Vector2 pos)
+        {
+            return new Vector2(pos.X * CellSize, pos.Y * CellSize);
+        }
+    }
+}

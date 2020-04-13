81eab37f7bc9323eeabb88e76b0312c2e6900d50
Tashkent <17104706+ototashkent@users.noreply.github.com>
2020-04-13 05:47:36 -0400

    you made a mistake changing strings in your half

--- IWETD.Game.Tests/Visual/TestSceneRoomFile.cs
+++ IWETD.Game.Tests/Visual/TestSceneRoomFile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using IWETD.Game.Graphics.Graphs;
 using IWETD.Game.IO;
 using IWETD.Game.Objects;
 using IWETD.Game.Screens;
@@ -12,6 +13,7 @@ using osu.Framework.Screens;
 using osu.Framework.Testing;
 using osu.Framework.Utils;
 using osuTK;
+using osuTK.Graphics;
 
 
 namespace IWETD.Game.Tests.Visual
@@ -19,6 +21,7 @@ namespace IWETD.Game.Tests.Visual
     public class TestSceneRoomFile : TestScene
     {
         private Room _room;
+        private Grid Grid => _room.ObjectGrid;
         private RoomManager _fileManager = new RoomManager(Path.Combine(Directory.GetCurrentDirectory(), "data/rooms/"));
 
         public TestSceneRoomFile()
@@ -34,39 +37,12 @@ namespace IWETD.Game.Tests.Visual
             AddUntilStep("wait for load", () => _room.IsLoaded);
             AddAssert("Check ID", () => _room.Id == 0);
 
-            AddStep("Add objects", () =>
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    _room.Objects.Add(new DrawableGameObject(new GameObject
-                    {
-                        X = RNG.Next(513),
-                        Y = RNG.Next(513)
-                    }));
-                }
-            });
-
-            AddAssert("Object size is correct", () => _room.Objects.Count == 10);
-            AddAssert("Can convert to string", () => !string.IsNullOrEmpty(_room.ToString()));
-            AddStep("Log string", () => Logger.Log(_room.ToString()));
-
-            AddStep("Add more objects", () =>
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    _room.Objects.Add(new DrawableGameObject(new GameObject
-                    {
-                        X = RNG.Next(513),
-                        Y = RNG.Next(513)
-                    }));
-                }
-            });
-
-            AddAssert("Object size is correct", () => _room.Objects.Count == 110);
             AddAssert("Can convert to string", () => !string.IsNullOrEmpty(_room.ToString()));
             AddStep("Log string", () => Logger.Log(_room.ToString()));
 
             AddStep("Render room file", () => _room.Render());
+
+            AddStep("yes", () => Logger.Log(_room.Objects.Count.ToString()));
         }
     }
 }

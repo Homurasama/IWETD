6d3833d39756d2069c4943285366a4987714eec6
Noah <altenyt@gmail.com>
2020-04-13 07:05:48 -0400

    Test saves and fix bugs

--- IWETD.Game/IO/Saves/SaveFile.cs
+++ IWETD.Game/IO/Saves/SaveFile.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using IWETD.Game.Attributes;
 using IWETD.Game.Interfaces;
 
 namespace IWETD.Game.IO.Saves
 {
     public class SaveFile : ISaveFile
     {
+        [GameProperty]
         public int Deaths { get; set; } = 0;
+        
+        [GameProperty]
         public int TimeSpent { get; set; } = 0;
+        
+        [GameProperty]
         public int RoomNumber { get; set; } = 0;
+        
+        [GameProperty]
         public int CheckpointId { get; set; } = 0;
 
-        public string ToString() => $"{Deaths}|{TimeSpent}|{RoomNumber}|{CheckpointId}";
+        public override string ToString() => $"{Deaths}|{TimeSpent}|{RoomNumber}|{CheckpointId}";
     }
 }

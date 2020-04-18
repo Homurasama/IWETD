53afdc2aa0baae1cb886ada91d293b7789e07fb3
Homura <17104706+ototashkent@users.noreply.github.com>
2020-04-17 21:06:21 -0400

    Add Stage

---
+++ IWETD.Game/Screens/IStage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IWETD.Game.Screens
+{
+    public interface IStage
+    {
+        string Name { get; }
+
+        string Directory { get; set; }
+    }
+    }
+}
---
+++ IWETD.Game/Screens/Stage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IWETD.Game.IO;
+using IWETD.Game.Screens.Rooms;
+
+namespace IWETD.Game.Screens
+{
+    public class Stage : IStage
+    {
+        public string Name { get; }
+
+        public Store<Room> Rooms = new Store<Room>();
+
+        public Room CurrentRoom;
+
+        public string Directory { get; set; }
+
+        public RoomManager RoomManager { get; set; }
+
+        public Stage(string stageDirectory)
+        {
+            RoomManager = new RoomManager(stageDirectory);
+            Directory = stageDirectory;
+        }
+
+        public void SwitchRoom(int index)
+        {
+            CurrentRoom?.Dispose();
+            CurrentRoom = Rooms[index];
+        }
+    }
+}

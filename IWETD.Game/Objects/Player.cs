87b0d729f4081471bd7bd5c70156f50fb079392f
Noah <altenyt@gmail.com>
2020-04-18 01:16:07 -0400

    Fix some collision issues

--- IWETD.Game/Objects/Player.cs
+++ IWETD.Game/Objects/Player.cs
@@ -39,8 +39,10 @@ namespace IWETD.Game.Objects
 
         public SaveFile SaveFile => SaveManager.Read(CurrentSaveFile);
         #endregion
+
+        private Vector2 _momentun;
         
-        private Room _room { get; set; }
+        private Room Room { get; set; }
 
         public Player(GameObject gameObject)
             : base(gameObject)
@@ -59,39 +61,40 @@ namespace IWETD.Game.Objects
             return true;
         }
         #endregion
-        
-        private float _direction;
 
         public void SetRoom(Room room, Vector2 position)
         {
-            _room = room;
+            Room = room;
 
             if (room.IsLoaded != true)
                 return;
 
             Position = position;
 
-            _room.AddPlayer(this);
+            Room.AddPlayer(this);
         }
 
         protected override void Update()
         {
-            List<Drawable> getSurrounding = GetSurroundingDrawables();
-            
-            /* foreach (Drawable drawable in getSurrounding)
-            {
-                //Console.WriteLine($"{drawable.X} - {drawable.Y} | Player: {this.X} - {this.Y}");
-            }*/
-
             if (!IsColliding())
             {
-                X += _direction;
-                Y = Math.Clamp(Y + 0.35f, 0, _room.ObjectGrid.Size.Y);
+                X += _momentun.X;
+                Y = Math.Clamp(Y + 0.35f, 0, Room.ObjectGrid.Size.Y);
             }
             else
             {
-                X -= 0.35f;
-                Y -= 1f;
+                // let's check where are we colliding.
+                if (_momentun.X > 0)
+                    X += _momentun.X;
+                else if (_momentun.X < 0)
+                    X -= -_momentun.X;
+
+                if (_momentun.Y > 0)
+                    Y += _momentun.Y;
+                else if (_momentun.Y < 0)
+                    Y -= _momentun.Y;
+
+                Console.WriteLine(_momentun);
             }
 
 
@@ -100,7 +103,7 @@ namespace IWETD.Game.Objects
         
         private bool IsColliding()
         {
-            foreach (var obj in _room.Objects)
+            foreach (var obj in Room.Objects)
             {
                 if (BoundingBox.IntersectsWith(obj.BoundingBox))
                     return true;
@@ -114,37 +117,15 @@ namespace IWETD.Game.Objects
             switch (action)
             {
                 case PlayerAction.Left:
-                    _direction = -0.35f;
+                    _momentun.X -= 0.35f;
                     break;
                 
                 case PlayerAction.Right:
-                    _direction = 0.35f;
+                    _momentun.X += 0.35f;
                     break;
             }
 
             return true;
-        }
-
-        public List<Drawable> GetSurroundingDrawables()
-        {
-            List<Drawable> surrounding = new List<Drawable>();
-
-            for (int i = 0; i < _room.Content.Count; i++)
-            {
-                // X
-                if ((this.X - _room.Content[i].X) < 0)
-                {
-                    surrounding.Add(_room.Content[i]);
-                }
-
-                // Y
-                if ((this.Y - _room.Content[i].Y) < 0)
-                {
-                    surrounding.Add(_room.Content[i]);
-                }
-            }
-
-            return surrounding;
         }
 
         public void OnReleased(PlayerAction action)
@@ -153,11 +134,11 @@ namespace IWETD.Game.Objects
             {
                 // the rest will break the direction regardless of it being a movement command or not.
                 case PlayerAction.Left:
-                    _direction = 0f;
+                    _momentun.X += 0.35f;
                     break;
                 
                 case PlayerAction.Right:
-                    _direction = 0f;
+                    _momentun.X -= 0.35f;
                     break;
             }
         }

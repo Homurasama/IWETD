d53309d28577e288899d8f0a20d10b9757194182
Noah <altenyt@gmail.com>
2020-08-02 00:58:26 -0400

    Remove l'old terrible code

--- IWETD.Game/Objects/Player.cs
+++ IWETD.Game/Objects/Player.cs
@@ -40,39 +40,18 @@ namespace IWETD.Game.Objects
 
         public SaveFile SaveFile => SaveManager.Read(CurrentSaveFile);
         #endregion
-
-        private Vector2 _momentun;
+        
+        private Vector2 velocity = Vector2.Zero;
+        private float gravity = 0.03f;
+        private float walkSpeed = 2;
         
         private Room Room { get; set; }
-
-        private Box _topCollision;
-        private Box _botCollision;
-        private Box _lefCollision;
-        private Box _rigCollision;
 
         public Player(GameObject gameObject)
             : base(gameObject)
         {
             Colour = Color4.Red;
-
-            AddInternal(CalculatePosition(_topCollision = new Box(), Vector2.UnitY).With(d => d.Height = 3));
-            AddInternal(CalculatePosition(_botCollision = new Box(), -Vector2.UnitY).With(d => d.Height = 3));
-            AddInternal(CalculatePosition(_rigCollision = new Box(), Vector2.UnitX).With(d => d.Width = 3));
-            AddInternal(CalculatePosition(_lefCollision = new Box(), -Vector2.UnitX).With(d => d.Width = 3));
         }
-
-        // we only want to check for 1px
-        private Drawable CalculatePosition(Drawable drawable, Vector2 position) =>
-            drawable.With(d =>
-            {
-                d.Size = Size;
-
-                if (position.X < 0 || position.Y < 0)
-                {
-                    d.Anchor = Anchor.BottomRight;
-                    d.Origin = Anchor.BottomRight;
-                }
-            });
 
         #region Events
         public virtual bool OnDeath() => true;
@@ -96,78 +75,12 @@ namespace IWETD.Game.Objects
             Position = position;
 
             Room.AddPlayer(this);
-            _momentun.Y = 0.35f;
         }
 
         protected override void UpdateAfterChildren()
         {
-            var colliding = IsColliding();
-
-            if (!colliding.Item1)
-            {
-                X += _momentun.X;
-                Y = Math.Clamp(Y + _momentun.Y, 0, Room.ObjectGrid.Size.Y);
-            }
-            else
-            {
-                // let's check where are we colliding.
-                foreach (var type in colliding.Item2)
-                {
-                    switch (type)
-                    {
-                        case GravityType.Up:
-                            Y -= 0.35f;
-                            
-                            break;
-                        
-                        case GravityType.Down:
-                            Y += 0.35f;
-                            
-                            break;
-                        
-                        case GravityType.Left:
-                            X -= 0.35f;
-                            
-                            break;
-                        
-                        case GravityType.Right:
-                            X += 0.35f;
-                            
-                            break;
-                    }
-                }
-            }
-
+            
             base.UpdateAfterChildren();
-        }
-        
-        private (bool, GravityType[]) IsColliding()
-        {
-            foreach (var obj in Room.Objects)
-            {
-                if (Hitbox.ScreenSpaceDrawQuad.AABBFloat.IntersectsWith(obj.ScreenSpaceDrawQuad.AABBFloat))
-                {
-                    var top = _topCollision.ScreenSpaceDrawQuad.AABBFloat.IntersectsWith(obj.ScreenSpaceDrawQuad.AABBFloat);
-                    var bot = _botCollision.ScreenSpaceDrawQuad.AABBFloat.IntersectsWith(obj.ScreenSpaceDrawQuad.AABBFloat);
-                    var lef = _lefCollision.ScreenSpaceDrawQuad.AABBFloat.IntersectsWith(obj.ScreenSpaceDrawQuad.AABBFloat);
-                    var rig = _rigCollision.ScreenSpaceDrawQuad.AABBFloat.IntersectsWith(obj.ScreenSpaceDrawQuad.AABBFloat);
-                    List<GravityType> type = new List<GravityType>();
-                    
-                    if (top)
-                        type.Add(GravityType.Up);
-                    if (bot)
-                        type.Add(GravityType.Down);
-                    
-                    if (lef)
-                        type.Add(GravityType.Left);
-                    if (rig)
-                        type.Add(GravityType.Right);
-                    
-                    return (true, type.ToArray());
-                }
-            }
-
-            return (false, Array.Empty<GravityType>());
         }
 
         public bool OnPressed(PlayerAction action)
@@ -175,11 +88,12 @@ namespace IWETD.Game.Objects
             switch (action)
             {
                 case PlayerAction.Left:
-                    _momentun.X += 0.35f;
                     break;
                 
                 case PlayerAction.Right:
-                    _momentun.X -= 0.35f;
+                    break;
+                
+                case PlayerAction.Jump:
                     break;
             }
 
@@ -190,13 +104,13 @@ namespace IWETD.Game.Objects
         {
             switch (action)
             {
-                // the rest will break the direction regardless of it being a movement command or not.
                 case PlayerAction.Left:
-                    _momentun.X -= 0.35f;
                     break;
                 
                 case PlayerAction.Right:
-                    _momentun.X += 0.35f;
+                    break;
+                
+                case PlayerAction.Jump:
                     break;
             }
         }
